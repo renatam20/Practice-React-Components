ecd14fe65e514497fbfad2bfadc293dfee6eb2aa
renatam20 <65694713+renatam20@users.noreply.github.com>
2020-08-05 23:48:21 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+import App from "./App";
+
+<Heading />;
+
+ReactDOM.render(<App />, document.getElementById("root"));
